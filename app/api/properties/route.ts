82b325c11df4434cffa7b047663113b6862bfdc0
kylezap <kzapcic+github@gmail.com>
2024-11-22 22:30:12 -0500

    create basic get all properties route

---
+++ app/api/properties/route.ts
@@ -0,0 +1,8 @@
+import { NextResponse } from 'next/server';
+import { db } from './db/connection';
+import { properties } from './db/schema';
+
+export async function GET() {
+    const allProperties = await db.select().from(properties);
+    return NextResponse.json(allProperties);
+}
